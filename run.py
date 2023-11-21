abf1e1483fad6ea1d5d801e59058cc6b480e2e73
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2023-11-21 20:32:56 +0100

    adding Wonder cabinet and Wonder Cabinet Inventory clases. Creating four attributes to the Wonder Cabinet Item - name, description, origin and care tips

--- run.py
+++ run.py
@@ -1,16 +1,14 @@
+import json
 import gspread
-from google.auth import Credentials
+from google.auth import credentials
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
-
-CREDS = Credentials.from_service_account_file("creds.json")
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("wonder_index")
+]
+CREDS = service_account.Credentials.from_service_account_file("creds.json", scopes=SCOPE)
+GSPREAD_CLIENT = gspread.authorize(CREDS)
 
 
-print("Hello")
+
