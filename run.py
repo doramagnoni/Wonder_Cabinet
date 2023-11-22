ccb6f4645b15bb0953acaeb15958afa9bff049d7
doramagnoni <137621206+doramagnoni@users.noreply.github.com>
2023-11-22 15:09:52 +0100

    fixing syntax mistakes and deploying to heroku

--- run.py
+++ run.py
@@ -1,45 +1,44 @@
 import json
 
 class WonderCabinetItem:
-    def __init__(self, name, description, origin, care_tips):
+    def __init__(self, name, description, origin):
         self.name = name
         self.description = description
         self.origin = origin
-        self.care_tips = care_tips
-
+       
 class WonderCabinetInventory:
     def __init__(self, file_path="wonder_cabinet_inventory.json"):
         self.file_path = file_path
         self.inventory = self.load_inventory()
 
-def load_inventory(self):
-    try:
-        with open(self.file_path, "r") as file:
-            data = json.load(file)
-            return [WonderCabinetItem(**item) for item in data]
-    except (FileNotFoundError, json.JSONDecodeError):
-        return []
+    def load_inventory(self):
+        try:
+            with open(self.file_path, "r") as file:
+                data = json.load(file)
+                return [WonderCabinetItem(**item) for item in data]
+        except (FileNotFoundError, json.JSONDecodeError):
+            return []
     
-def save_inventory(self):
-    data = [{"name": item.name, "description": item.description, "origin": item.origin, "care_tips": item.care_tips}
-            for item in self.inventory]
-    with open(self.file_path, "w") as file:
-        json.dump(data, file, indent=2)
+    def save_inventory(self):
+        data = [{"name": item.name, "description": item.description, "origin": item.origin}
+                for item in self.inventory]
+        with open(self.file_path, "w") as file:
+            json.dump(data, file, indent=2)
 
-def add_item(self, item):
-    self.inventory.append(item)
-    self.save_inventory()
+    def add_item(self, item):
+        self.inventory.append(item)
+        self.save_inventory()
 
-def remove_item(self, item_name):
-    self.inventory = [item for item in self.inventory if item.name != item_name] 
-    self.save_inventory()
+    def remove_item(self, item_name):
+        self.inventory = [item for item in self.inventory if item.name != item_name] 
+        self.save_inventory()
 
-def search_items(self, keyword): 
-    return [item for item in self.inventory if keyword.lower() in item.name.lower()]
+    def search_items(self, keyword): 
+        return [item for item in self.inventory if keyword.lower() in item.name.lower()]
 
-def display_inventory(self):
-    for item in self.inventory:
-        print(f"{item.name}: {item.description}: {item.origin}: {item.care_tips}:")
+    def display_inventory(self):
+        for item in self.inventory:
+            print(f"Name: {item.name}, Description: {item.description}, Origin: {item.origin}")
         
 def main():
     cabinet_inventory = WonderCabinetInventory()
@@ -58,8 +57,7 @@ def main():
             name = input("Enter item name: ")
             description = input("Enter item description: ")
             origin = input("Enter the origin of the item: ")
-            care_tips = input("Enter any special care tips: ")
-            new_item = WonderCabinetItem(name, description, origin, care_tips)
+            new_item = WonderCabinetItem(name, description, origin)
             cabinet_inventory.add_item(new_item)
             
         elif choice == "2":
